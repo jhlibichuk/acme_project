00dd821f83af0380ebf4afa055d6e95d1ba18d88
Josh Hlibichuk <josh.hlibichuk@thegazette.com>
2021-06-02 17:00:44 -0500

    adjusting tests

--- test/integration/outreach_test.rb
+++ test/integration/outreach_test.rb
@@ -3,20 +3,20 @@ require 'test_helper'
 class OutreachTest < ActionDispatch::IntegrationTest
 
   test "outreach analyzer works" do
-   warm_engagement = Outreacher.determine_engagement(weather:"Sunny", temp: "77")
+   warm_engagement = Outreacher.determine_engagement(weather:"Clear Skies", temp: "77")
    assert_equal("Email", warm_engagement)
 
-   cool_engagement = Outreacher.determine_engagement(weather:"Sunny", temp: "35")
+   cool_engagement = Outreacher.determine_engagement(weather:"Clear Skies", temp: "35")
    assert_equal("Phone Call", cool_engagement)
 
    rainy_engagement = Outreacher.determine_engagement(weather:"Rain", temp: "76")
    assert_equal("Phone Call", rainy_engagement)
 
-   pleasant_engagement = Outreacher.determine_engagement(weather:"Cloudy", temp: "65")
+   pleasant_engagement = Outreacher.determine_engagement(weather:"Partly Cloudy", temp: "65")
    assert_equal("Text Message", pleasant_engagement)
 
-   unknown_engagement = Outreacher.determine_engagement(weather:"Cloudy", temp: "85")
-   assert_equal("??", unknown_engagement)
+   hot_cloudy_engagement = Outreacher.determine_engagement(weather:"Partly Cloudy", temp: "85")
+   assert_equal("Push Notification", hot_cloudy_engagement)
 
   end
 
