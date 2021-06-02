2afcaf78178c9a419b56ce722b473cd2513a6733
Josh Hlibichuk <josh.hlibichuk@thegazette.com>
2021-06-02 16:58:57 -0500

    moving location out to the application controller

--- app/controllers/acme_controller.rb
+++ app/controllers/acme_controller.rb
@@ -1,9 +1,15 @@
 class AcmeController < ApplicationController
 
   def index
-    @location = "Minneapolis, MN"
+    # returns an array of day/weather data hashes which looks like this:
+    # [{date: string, temperature: float, weather: string, icon: string, engagement: string},{}...]
+
     @days = OpenWeather.current_weather_data
-    @now = Time.now
+
+    # I'm okay calling this on pageload because it's cached...
+    # but I'd also consider grabbing data in a background job, then storing that data
+    # and then querying the DB to get the results intead
+    # which I think would be more reliable long-term
 
   end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_location
+
+  # default location
+  # for other users/use cases we'd query our user data or data about where we're targeting
+  # maybe even from the UI
+  # for now though...
+  
+  def set_location
+    @location = "Minneapolis, MN"
+  end
+
 end
